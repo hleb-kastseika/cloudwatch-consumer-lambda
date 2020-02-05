1d248f52f48a393b15d00071a65590c67d15d36c
Hleb Kastseika <hkastseika@edmunds.com>
2020-02-05 15:04:41 +0300

    Replace Tinylog with JUL

--- src/main/java/gk/logconsumer/LogHandler.java
+++ src/main/java/gk/logconsumer/LogHandler.java
@@ -1,26 +1,15 @@
 package gk.logconsumer;
 
-import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import gk.logconsumer.model.CloudWatchLogEvent;
 import gk.logconsumer.model.CloudWatchPutRequest;
 import gk.logconsumer.model.LambdaResponse;
-import gk.logconsumer.model.CloudWatchLogEvent;
-import org.pmw.tinylog.Configurator;
-import org.pmw.tinylog.Level;
-import org.pmw.tinylog.Logger;
-import org.pmw.tinylog.writers.ConsoleWriter;
+import java.util.logging.Logger;
 
 public class LogHandler implements RequestHandler<CloudWatchPutRequest, LambdaResponse> {
-    private static final String LOG_MESSAGE_TEMPLATE = "{date:yyyy-MM-dd HH:mm:ss} {level} {class}.{method}():{line} - {message}";
+    private static Logger LOGGER = Logger.getGlobal();
     private PayloadDecoder decoder = new PayloadDecoder();
-
-    static {
-        Configurator.defaultConfig()
-                .writer(new ConsoleWriter())
-                .level(Level.INFO)
-                .formatPattern(LOG_MESSAGE_TEMPLATE)
-                .activate();
-    }
 
     @Override
     public LambdaResponse handleRequest(CloudWatchPutRequest request, Context context) {
@@ -28,7 +17,7 @@ public class LogHandler implements RequestHandler<CloudWatchPutRequest, LambdaRe
         var logs = logEvents.getLogEvents();
 
         for (CloudWatchLogEvent log : logs) {
-            Logger.info("~~~~~~ Transmitted log = " + log);
+            LOGGER.info(log.toString());
         }
         return new LambdaResponse(); //TODO possibly remove
     }
